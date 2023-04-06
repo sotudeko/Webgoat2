e031ccada48a86c143df357f7dca04db259f52fe
Sola Otudeko <so2d@icloud.com>
2023-04-06 15:50:05 +0100

    37

--- nexus-iq-scan.sh
+++ nexus-iq-scan.sh
@@ -3,4 +3,4 @@
 echo Hello World
 echo running Nexus IQ scan
 
-nexus-iq-cli -s http://localhost:8070 -a zwwu7vYv:8Uy6pVMKKLaZmyWjgwBuCAW4kUoVkqfAW7f1VWmzn2TU -i webgoat2 target/WebGoat-*.war
+./nexus-iq-cli -s http://localhost:8070 -a zwwu7vYv:8Uy6pVMKKLaZmyWjgwBuCAW4kUoVkqfAW7f1VWmzn2TU -i webgoat2 target/WebGoat-*.war
